31d22844d73cdb5f9a242621252ddf0ff485b72c
jonsson11 <jooonds11@gmail.com>
2025-02-27 10:13:56 +0100

    Use of rememberSaveable in order to save the state of the app despite modificacions like screen rotation, language switching...

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -9,14 +9,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 
 
 @Composable
 fun WaterCounter (modifier : Modifier = Modifier) {
     Column (modifier = modifier.padding(16.dp)){
-        var count by remember{ mutableStateOf(0) }
+        var count by rememberSaveable{ mutableStateOf(0) }
 
         if (count > 0) {
             Text(text = "You drank $count glasses.")
