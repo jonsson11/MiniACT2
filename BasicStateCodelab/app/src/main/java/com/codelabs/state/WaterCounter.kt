3277738a71d98c28331435e6ef3635db01d15e71
jonsson11 <jooonds11@gmail.com>
2025-02-27 10:12:36 +0100

    Modify WaterCounter.kt in order to have also a limit of 10 water glasses.

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -18,9 +18,12 @@ fun WaterCounter (modifier : Modifier = Modifier) {
     Column (modifier = modifier.padding(16.dp)){
         var count by remember{ mutableStateOf(0) }
 
-        Text(text = "You drinked $count glasses.")
-        Button(onClick = {count++}, modifier.padding(top = 8.dp)) {
+        if (count > 0) {
+            Text(text = "You drank $count glasses.")
+        }
+        Button(onClick = {count++}, modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one glass")
         }
+
     }
 }
